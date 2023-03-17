190a48c1179c3b5a07269bc204cdb7648b58483f
Kanchanapohn <117479061+Kanchanapohn@users.noreply.github.com>
2023-03-18 04:11:10 +0700

    Update Exercise4_Kanchanapohn_P.py

--- Exercise4_Kanchanapohn_P.py
+++ Exercise4_Kanchanapohn_P.py
@@ -1,6 +1,6 @@
 A = 25 #Foundation English
 B = 35 #General Business 
-c = 85 #Introduction to Computer Systems 
+C = 85 #Introduction to Computer Systems 
 D = 75 #Computer Programming
 
 print("--- Your Score ---")

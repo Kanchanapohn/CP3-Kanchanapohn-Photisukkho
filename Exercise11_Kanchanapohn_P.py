2f7d50095a0590ca07ea2ec9b5d7146965f2daf0
Kanchanapohn <117479061+Kanchanapohn@users.noreply.github.com>
2023-03-25 03:43:00 +0700

    Create Exercise11_Kanchanapohn_P.py

---
+++ Exercise11_Kanchanapohn_P.py
@@ -0,0 +1,9 @@
+print("input")
+number = int(input("Please specify the number of pyramid floors : "))
+print("output")
+for x in range(number) : 
+    for y in range(number-x-1) :
+        print(" ",end=" ")
+    for z in range(2*x+1) :
+        print("*", end=" ")
+    print()  
